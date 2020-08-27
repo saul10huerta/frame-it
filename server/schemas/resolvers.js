b7a58c5bb27bd360f7e95e579268298fe84d9d60
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-08-26 22:12:18 -0500

    trying to get signup or login working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,54 @@ const { User, Item, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 
 
-
-
 const resolvers = {
+    Query: {
+        user: async (parent, args, context) => {
+            if (context.user) {
+              const user = await User.findById(context.user._id).populate({
+                populate: 'item'
+              });
+      
+              //user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+      
+              return user;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+        },
+        users: async () => {
+            return User.find()
+            .select('-__v -password');
+            // .populate('friends')
+            // .populate('thoughts');
+        },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return { token, user };
+        }
+          
+    }
 };
+
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
   type Item {
     title: String
     image: String
@@ -10,21 +9,25 @@ const typeDefs = gql`
     status: String
     likes: Int
   }
-
   type User {
+    _id: ID
+    username: String
     firstName: String
     lastName: String
     email: String
     items: [Item]
   }
-
   type Auth {
     token: ID
     user: User
   }
-
   type Query {
     user: User
+    users: [User]
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!, firstName: String!, lastName: String!): Auth
   }
   `;
 
