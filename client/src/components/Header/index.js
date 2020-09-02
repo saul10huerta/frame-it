8db03355491c6c8d4528b4dd068701e98ac24006
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-09-01 22:57:42 -0500

    able to get logout to work for logged in users

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import Auth from '../../utils/auth';
+import Auth from '../../utils/auth';
 
 const Header = () => {
 
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
     return (
         <header>
             <Link to='/'>
                 <h1>FrameIt</h1>
             </Link>
 
-            <Link to='/login'>
-                <h4>Login</h4>
-            </Link>
-
-            <Link to='/signup'>
-                <h4>Signup</h4>
-            </Link>
-
-            <Link to='/profile'>
-                <h4>Profile</h4>
-            </Link>
-
-
+            {Auth.loggedIn() ? (
+            <>
+              <Link to='/profile'><h4>Profile</h4></Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+            ) : (
+                <>
+                    <Link to='/login'><h4>Login</h4></Link>
+                    <Link to='/signup'><h4>Signup</h4></Link>
+                </>
+            )}
         </header>
     )
 }
